3ba2f58df5e20b358edc05123db3b43843f17afc
Vishal P <vishalprasanna11@gmail.com>
2021-05-17 08:58:56 +0530

    Final Update

--- Application/Scripts/recipe.js
+++ Application/Scripts/recipe.js
@@ -3,7 +3,6 @@ const mealList = document.getElementById('meal');
 const mealDetailsContent = document.querySelector('.meal-details-content');
 const recipeCloseBtn = document.getElementById('recipe-close-btn');
 
-// event listeners
 searchBtn.addEventListener('click', getMealList);
 mealList.addEventListener('click', getMealRecipe);
 recipeCloseBtn.addEventListener('click', () => {
@@ -11,22 +10,22 @@ recipeCloseBtn.addEventListener('click', () => {
 });
 
 
-// get meal list that matches with the ingredients
 function getMealList(){
     let searchInputTxt = document.getElementById('search-input').value.trim();
-    fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?i=${searchInputTxt}`)
+    fetch(`https://api.spoonacular.com/recipes/complexSearch?query="${searchInputTxt}"&apiKey=6311ae6ba06349b9b24ebc42b19fb517`)
     .then(response => response.json())
-    .then(data => {
+    .then(data =>
+     {
         let html = "";
-        if(data.meals){
-            data.meals.forEach(meal => {
+        if(data.results){
+            data.results.forEach(meal => {
                 html += `
-                    <div class = "meal-item" data-id = "${meal.idMeal}">
+                    <div class = "meal-item" data-id = "${meal.id}">
                         <div class = "meal-img">
-                            <img src = "${meal.strMealThumb}" alt = "food">
+                            <img src = "${meal.image}" alt = "food">
                         </div>
                         <div class = "meal-name">
-                            <h3>${meal.strMeal}</h3>
+                            <h3>${meal.title}</h3>
                             <a href = "#" class = "recipe-btn">Get Recipe</a>
                         </div>
                     </div>
@@ -43,33 +42,30 @@ function getMealList(){
 }
 
 
-// get recipe of the meal
 function getMealRecipe(e){
     e.preventDefault();
     if(e.target.classList.contains('recipe-btn')){
         let mealItem = e.target.parentElement.parentElement;
-        fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${mealItem.dataset.id}`)
+        fetch(`https://api.spoonacular.com/recipes/${mealItem.dataset.id}/information?includeNutrition=false&apiKey=6311ae6ba06349b9b24ebc42b19fb517`)
         .then(response => response.json())
-        .then(data => mealRecipeModal(data.meals));
+        .then(data => mealRecipeModal(data));
     }
 }
 
-// create a modal
 function mealRecipeModal(meal){
     console.log(meal);
-    meal = meal[0];
     let html = `
-        <h2 class = "recipe-title">${meal.strMeal}</h2>
-        <p class = "recipe-category">${meal.strCategory}</p>
+        <h2 class = "recipe-title">${meal.title}</h2>
+        <p class = "recipe-minutes">Ready in ${meal.readyInMinutes} minutes</p>
         <div class = "recipe-instruct">
             <h3>Instructions:</h3>
-            <p>${meal.strInstructions}</p>
+            <p>${meal.instructions}</p>
         </div>
         <div class = "recipe-meal-img">
-            <img src = "${meal.strMealThumb}" alt = "">
+            <img src = "${meal.image}" alt = "">
         </div>
         <div class = "recipe-link">
-            <a href = "${meal.strYoutube}" target = "_blank">Watch Video</a>
+            <a href = "${meal.sourceUrl}" target = "_blank">Click here to know more </a>
         </div>
     `;
     mealDetailsContent.innerHTML = html;
